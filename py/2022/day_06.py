34bdcd61f58f5f8def7d219209b3d12123d38e2b
Xithrius <xithrius@gmail.com>
2022-12-05 21:36:29 -0800

    Fixed up day 6

--- py/2022/day_06.py
+++ py/2022/day_06.py
@@ -10,15 +10,13 @@ DATA = parse_raw()
 
 def part_one():
     for i in range(0, len(DATA) - 4):
-        element = DATA[i:i+4]
-        if (len(set(element)) == 4):
+        if len(set(DATA[i:i+4])) == 4:
             return i + 4
 
 def part_two():
     for i in range(0, len(DATA) - 14):
-        element = DATA[i:i+14]
-        if (len(set(element)) == 14):
-            return i + 14
+        if len(set(DATA[i:i+14])) == 14:
+            return i + 14 
 
 aoc_lube.submit(year=2022, day=6, part=1, solution=part_one)
 aoc_lube.submit(year=2022, day=6, part=2, solution=part_two)
