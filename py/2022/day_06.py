a35aca33a5fbaebb7b5efb9ff7493c0c78319e97
Xithrius <xithrius@gmail.com>
2022-12-05 21:08:15 -0800

    Done with day 6

--- py/2022/day_06.py
+++ py/2022/day_06.py
@@ -4,15 +4,21 @@ RAW = aoc_lube.fetch(year=2022, day=6)
 print(RAW)
 
 def parse_raw():
-    ...
+    return list(RAW.strip())
 
 DATA = parse_raw()
 
 def part_one():
-    ...
+    for i in range(0, len(DATA) - 4):
+        element = DATA[i:i+4]
+        if (len(set(element)) == 4):
+            return i + 4
 
 def part_two():
-    ...
+    for i in range(0, len(DATA) - 14):
+        element = DATA[i:i+14]
+        if (len(set(element)) == 14):
+            return i + 14
 
 aoc_lube.submit(year=2022, day=6, part=1, solution=part_one)
 aoc_lube.submit(year=2022, day=6, part=2, solution=part_two)
