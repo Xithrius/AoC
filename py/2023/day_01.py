21e56bc2bac78073e8ba5a0c4050d4d947495b26
Xithrius <xithrius@gmail.com>
2023-12-01 23:02:08 -0800

    Formatted day 1, completed day 2

--- py/2023/day_01.py
+++ py/2023/day_01.py
@@ -2,16 +2,22 @@ import aoc_lube
 
 RAW = aoc_lube.fetch(year=2023, day=1)
 
+
 def parse_raw():
-    return [list(x) for x in RAW.split('\n')]
+    return [list(x) for x in RAW.split("\n")]
+
 
 DATA = parse_raw()
 
+
 def part_one():
-    return sum(map(lambda z: int(z[0] + z[-1]), [[j for j in x if j.isdigit()] for x in DATA]))
+    return sum(
+        map(lambda z: int(z[0] + z[-1]), [[j for j in x if j.isdigit()] for x in DATA])
+    )
 
 
 digits = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+
 
 def part_two():
     d: str = ["".join(a) for a in DATA]
@@ -23,7 +29,7 @@ def part_two():
                 inner.append((i, b))
         for digit in digits:
             for x in range(len(a) - len(digit) + 1):
-                if a[x:x + len(digit)] == digit:
+                if a[x : x + len(digit)] == digit:
                     inner.append((x, str(digits.index(digit) + 1)))
 
         lst = list(sorted(inner, key=lambda z: z[0]))
--- py/2023/day_02.py
+++ py/2023/day_02.py
@@ -1,18 +1,77 @@
 import aoc_lube
+import itertools
 
 RAW = aoc_lube.fetch(year=2023, day=2)
-print(RAW)
+
 
 def parse_raw():
-    ...
+    return [x for x in RAW.split("\n")]
+
 
 DATA = parse_raw()
 
+
 def part_one():
-    ...
+    check = {
+        "red": 12,
+        "green": 13,
+        "blue": 14,
+    }
+
+    ids = 0
+
+    for line in DATA:
+        id = int(line.split(":")[0].split()[-1])
+        bags = list(
+            map(
+                lambda z: [int(z[0]), z[1]],
+                [
+                    x.strip().split()
+                    for x in itertools.chain(
+                        *[j.split(", ") for j in line.split(":")[1].split(";")]
+                    )
+                ],
+            )
+        )
+
+        valid = True
+
+        for bag in bags:
+            amount, color = bag
+            if amount > check[color]:
+                valid = False
+                break
+
+        if valid:
+            ids += id
+
+    return ids
+
 
 def part_two():
-    ...
+    total = 0
+    for line in DATA:
+        bags = list(
+            map(
+                lambda z: [int(z[0]), z[1]],
+                [
+                    x.strip().split()
+                    for x in itertools.chain(
+                        *[j.split(", ") for j in line.split(":")[1].split(";")]
+                    )
+                ],
+            )
+        )
+
+        out = 1
+
+        for c in ["red", "blue", "green"]:
+            out *= max([amount for amount, color in bags if color == c])
+
+        total += out
+
+    return total
+
 
 aoc_lube.submit(year=2023, day=2, part=1, solution=part_one)
 aoc_lube.submit(year=2023, day=2, part=2, solution=part_two)
